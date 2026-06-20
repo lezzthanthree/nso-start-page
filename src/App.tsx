9b28add6aaf564b5711f7c60d976855e37f7cb4e
Smilie Pop <arisatominato48@gmail.com>
2026-06-20 13:44:24 +0800

    Added close button and disable image anti-aliasing

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Window } from "./components/Window";
 function App() {
     return (
         <div className="w-screen h-screen bg-[url('/img/nso_bg_1080.png')] flex flex-col items-center justify-center">
-            <div className="flex flex-1" id="desktop">
+            <div className="flex flex-1 w-full" id="desktop">
                 <Window title="Test">
-                    <div className="flex flex-row items-center justify-center flex-1">
+                    <div className="flex flex-row items-center justify-center flex-1 w-180">
                         Test
                     </div>
                 </Window>
--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -12,7 +12,11 @@ export const Window = (prop: WindowProps) => {
     const nodeRef = useRef(null);
 
     return (
-        <Draggable nodeRef={nodeRef} handle="#title-bar">
+        <Draggable
+            nodeRef={nodeRef}
+            handle="#title-bar"
+            defaultPosition={{ x: 100, y: 100 }}
+        >
             <div
                 className="absolute border-2 border-nso-purple bg-nso-cyan px-1 pt-1 box-border flex flex-col shadow-[4px_4px_#4d23cf55] min-w-64 min-h-64"
                 ref={nodeRef}
@@ -21,24 +25,23 @@ export const Window = (prop: WindowProps) => {
                     className="flex flex-row items-center bg-nso-light-pink border-2 border-nso-purple box-border p-1 gap-1"
                     id="title-bar"
                 >
-                    <div className="bg-nso-purple min-h-4 min-w-4" />
-                    <span className="w-full text-[16px]/[16px] font-nso-dinkie-9px text-nso-purple">
+                    <div className="bg-nso-purple h-5 w-5 shrink-0" />
+                    <span className="w-full text-[20px]/[20px] font-nso-dinkie-9px text-nso-purple">
                         {title}
                     </span>
-                    <div className="flex gap-1">
-                        <div className="border-2 border-nso-purple min-h-4 min-w-4" />
-                        <div className="border-2 border-nso-purple min-h-4 min-w-4" />
-                        <div className="border-2 border-nso-purple min-h-4 min-w-4" />
+                    <div className="flex gap-1 h-5 shrink-0">
+                        {/* image is 20x20 */}
+                        <img src="img/button_close.png" className="w-5 h-5" />
                     </div>
                 </div>
                 <div className="bg-white border-2 border-nso-purple my-1 border-box flex flex-col flex-1">
                     {children}
                 </div>
                 <div className="flex flex-row">
-                    <div className="h-2 w-8 bg-nso-dark-pink border-2 border-b-0 border-box border-nso-purple" />
-                    <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
-                    <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
-                    <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
+                    <div className="h-2.5 w-12 bg-nso-dark-pink border-2 border-b-0 border-box border-nso-purple" />
+                    <div className="h-2 w-2 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
+                    <div className="h-2 w-2 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
+                    <div className="h-2 w-2 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
                 </div>
             </div>
         </Draggable>
